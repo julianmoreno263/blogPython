d9d3e3610b43be44e6dff5bdf26779291fc1123a
julian <julianmoreno263@gmail.com>
2026-06-02 16:47:56 -0500

    feat: creacion automatica de tablas antes de la primera peticion

--- run.py
+++ run.py
@@ -1,8 +1,15 @@
-from blogr import createApp
+from blogr import createApp, db
 
+app = createApp()
 
-if __name__=="__main__":
-    app=createApp()
+# Este decorador mágico creará las tablas automáticamente en Render
+@app.before_request
+def create_tables():
+    # Remueve el decorador después de ejecutarse la primera vez para no saturar el servidor
+    app.before_request_funcs[None].remove(create_tables)
+    db.create_all()
+
+if __name__ == '__main__':
     app.run()
 
 
